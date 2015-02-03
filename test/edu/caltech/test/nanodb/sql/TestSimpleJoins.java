e2bec0d69cca41e9b8e5d059d916b19dca6bae1c
Aleksander Bello <abello@caltech.edu>
2015-02-02 20:02:54 -0800

    Added right outer join tests

--- test/edu/caltech/test/nanodb/sql/TestSimpleJoins.java
+++ test/edu/caltech/test/nanodb/sql/TestSimpleJoins.java
@@ -131,11 +131,119 @@ public class TestSimpleJoins extends SqlTestCase {
      * ************************************************************
      */
 
+
     /**
      * ************************************************************
      * ********************* RIGHT OUTER JOIN TESTS ***************
      * ************************************************************
      */
+
+
+    /**
+     * This test performs a simple right outer join between two non-empty tables. Not every column
+     * should join.
+     */
+    public void testROJSimple() throws Throwable {
+        TupleLiteral[] expected = {
+                new TupleLiteral(10, 1, 1, 100),
+                new TupleLiteral(20, 2, 2, 200),
+                new TupleLiteral(30, 3, 3, 300),
+                new TupleLiteral(null, null, 4, 400),
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_1 AS t1 RIGHT OUTER JOIN test_joins_2 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+    /**
+     * This test performs an right outer join between a empty table (right) and a non-empty table.
+     * @throws Throwable
+     */
+    public void testROJEmptyRight() throws Throwable {
+        TupleLiteral[] expected = {
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_1 AS t1 RIGHT OUTER JOIN test_joins_empty AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+    /**
+     * This test performs an right outer join between a empty table (left) and a non-empty table.
+     * @throws Throwable
+     */
+    public void testROJEmptyLeft() throws Throwable {
+        TupleLiteral[] expected = {
+                new TupleLiteral(null, null, 1, 100),
+                new TupleLiteral(null, null, 2, 200),
+                new TupleLiteral(null, null, 3, 300),
+                new TupleLiteral(null, null, 4, 400),
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_empty AS t1 RIGHT OUTER JOIN test_joins_2 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+
+    /**
+     * This test performs a right outer join between two empty tables;
+     * @throws Throwable
+     */
+    public void testROJEmptyBoth() throws Throwable {
+        TupleLiteral[] expected = {
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_empty AS t1 RIGHT OUTER JOIN test_joins_empty_2 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+
+    /**
+     * This tests performs an right outer join between two nonempty tables where multiple rows of
+     * the right table would join.
+     * @throws Throwable
+     */
+    public void testROJEmptyDupRight() throws Throwable {
+        TupleLiteral[] expected = {
+            new TupleLiteral(10, 1, 1, 100),
+            new TupleLiteral(20, 2, 2, 200),
+            new TupleLiteral(20, 2, 2, 222),
+            new TupleLiteral(30, 3, 3, 300),
+            new TupleLiteral(null, null, 4, 400),
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_1 AS t1 RIGHT OUTER JOIN test_joins_dup_2 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+    /**
+     * This tests performs an right outer join between two nonempty tables where multiple rows of
+     * the left table would join.
+     * @throws Throwable
+     */
+    public void testROJEmptyDupLeft() throws Throwable {
+        TupleLiteral[] expected = {
+                new TupleLiteral(10, 1, 1, 100),
+                new TupleLiteral(20, 2, 2, 200),
+                new TupleLiteral(22, 2, 2, 200),
+                new TupleLiteral(30, 3, 3, 300),
+                new TupleLiteral(40, 4, 4, 400),
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_dup_3 AS t1 RIGHT OUTER JOIN test_joins_dup_4 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
 
     // TODO: Natural join tests (in different class)
     // TODO: "USING" tests (in different class)
