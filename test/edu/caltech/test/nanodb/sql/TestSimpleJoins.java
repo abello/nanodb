556027d7220579b83185aa71d8fdf7528d18edac
Aleksander Bello <abello@caltech.edu>
2015-02-02 20:14:19 -0800

    Added left outer join tests

--- test/edu/caltech/test/nanodb/sql/TestSimpleJoins.java
+++ test/edu/caltech/test/nanodb/sql/TestSimpleJoins.java
@@ -131,6 +131,112 @@ public class TestSimpleJoins extends SqlTestCase {
      * ************************************************************
      */
 
+    /**
+     * This test performs a simple left outer join between two non-empty tables. Not every column
+     * should join.
+     */
+    public void testLOJSimple() throws Throwable {
+        TupleLiteral[] expected = {
+                new TupleLiteral(0, 0, null, null),
+                new TupleLiteral(10, 1, 1, 100),
+                new TupleLiteral(20, 2, 2, 200),
+                new TupleLiteral(30, 3, 3, 300),
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_1 AS t1 LEFT OUTER JOIN test_joins_2 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+    /**
+     * This test performs a left outer join between a empty table (right) and a non-empty table.
+     * @throws Throwable
+     */
+    public void testLOJEmptyRight() throws Throwable {
+        TupleLiteral[] expected = {
+                new TupleLiteral(0, 0, null, null),
+                new TupleLiteral(10, 1, null, null),
+                new TupleLiteral(20, 2, null, null),
+                new TupleLiteral(30, 3, null, null),
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_1 AS t1 LEFT OUTER JOIN test_joins_empty AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+    /**
+     * This test performs a left outer join between a empty table (left) and a non-empty table.
+     * @throws Throwable
+     */
+    public void testLOJEmptyLeft() throws Throwable {
+        TupleLiteral[] expected = {
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_empty AS t1 LEFT OUTER JOIN test_joins_2 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+
+    /**
+     * This test performs a left outer join between two empty tables;
+     * @throws Throwable
+     */
+    public void testLOJEmptyBoth() throws Throwable {
+        TupleLiteral[] expected = {
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_empty AS t1 LEFT OUTER JOIN test_joins_empty_2 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+
+    /**
+     * This tests performs a left outer join between two nonempty tables where multiple rows of
+     * the right table would join.
+     * @throws Throwable
+     */
+    public void testLOJDupRight() throws Throwable {
+        TupleLiteral[] expected = {
+                new TupleLiteral(0, 0, null, null),
+                new TupleLiteral(10, 1, 1, 100),
+                new TupleLiteral(20, 2, 2, 200),
+                new TupleLiteral(20, 2, 2, 222),
+                new TupleLiteral(30, 3, 3, 300),
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_1 AS t1 LEFT OUTER JOIN test_joins_dup_2 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
+    /**
+     * This tests performs a left outer join between two nonempty tables where multiple rows of
+     * the left table would join.
+     * @throws Throwable
+     */
+    public void testLOJDupLeft() throws Throwable {
+        TupleLiteral[] expected = {
+                new TupleLiteral(10, 1, 1, 100),
+                new TupleLiteral(20, 2, 2, 200),
+                new TupleLiteral(22, 2, 2, 200),
+                new TupleLiteral(30, 3, 3, 300),
+                new TupleLiteral(40, 4, 4, 400),
+        };
+
+        CommandResult result;
+
+        result = server.doCommand("SELECT * FROM test_joins_dup_3 AS t1 LEFT OUTER JOIN test_joins_dup_4 AS t2 ON t1.b = t2.b", true);
+        assert checkUnorderedResults(expected, result);
+    }
+
 
     /**
      * ************************************************************
@@ -158,7 +264,7 @@ public class TestSimpleJoins extends SqlTestCase {
     }
 
     /**
-     * This test performs an right outer join between a empty table (right) and a non-empty table.
+     * This test performs a right outer join between a empty table (right) and a non-empty table.
      * @throws Throwable
      */
     public void testROJEmptyRight() throws Throwable {
@@ -172,7 +278,7 @@ public class TestSimpleJoins extends SqlTestCase {
     }
 
     /**
-     * This test performs an right outer join between a empty table (left) and a non-empty table.
+     * This test performs a right outer join between a empty table (left) and a non-empty table.
      * @throws Throwable
      */
     public void testROJEmptyLeft() throws Throwable {
@@ -206,11 +312,11 @@ public class TestSimpleJoins extends SqlTestCase {
 
 
     /**
-     * This tests performs an right outer join between two nonempty tables where multiple rows of
+     * This tests performs a right outer join between two nonempty tables where multiple rows of
      * the right table would join.
      * @throws Throwable
      */
-    public void testROJEmptyDupRight() throws Throwable {
+    public void testROJDupRight() throws Throwable {
         TupleLiteral[] expected = {
             new TupleLiteral(10, 1, 1, 100),
             new TupleLiteral(20, 2, 2, 200),
@@ -226,11 +332,11 @@ public class TestSimpleJoins extends SqlTestCase {
     }
 
     /**
-     * This tests performs an right outer join between two nonempty tables where multiple rows of
+     * This tests performs a right outer join between two nonempty tables where multiple rows of
      * the left table would join.
      * @throws Throwable
      */
-    public void testROJEmptyDupLeft() throws Throwable {
+    public void testROJDupLeft() throws Throwable {
         TupleLiteral[] expected = {
                 new TupleLiteral(10, 1, 1, 100),
                 new TupleLiteral(20, 2, 2, 200),
