97b74a4dfc42a8bea465ccfeeb2639ff3d7190f4
Aleksander Bello <abello@caltech.edu>
2015-03-06 00:06:42 -0800

    Automating first test

--- manual_tests/txn-test.py
+++ manual_tests/txn-test.py
@@ -1,12 +1,10 @@
 # looked up most of this stuff from SO
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, call
 
 proc = Popen(["./nanodb", ""], stdout=PIPE, stdin=PIPE)
 
 CREATE_TBL = "CREATE TABLE testwal (a INTEGER, b VARCHAR(30), c FLOAT);\n"
 
-stdout = proc.stdin.write(CREATE_TBL)
-print proc.communicate()[0]
 
 # BEGIN;
 INS_1 = "INSERT INTO testwal VALUES (1, 'abc', 1.2);\n"
@@ -21,5 +19,18 @@ BEGIN = "BEGIN;\n"
 COMMIT = "COMMIT;\n"
 FLUSH = "FLUSH;\n"
 CRASH = "CRASH;\n"
+
+def delete_files():
+    call(["rm", "../datafiles/*.log", "../datafiles/*.dat", "../datafiles/*.tbl"])
+
+
+delete_files()
+stdout = proc.stdin.write(CREATE_TBL)
+stdout = proc.stdin.write(INS_1)
+stdout = proc.stdin.write(INS_2)
+
+stdout = proc.stdin.write(SELECT)
+
+print proc.communicate()[0]
 
 
